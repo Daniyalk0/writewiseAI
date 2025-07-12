7477b811deb9d8c29fc8f283df010c54685c15d3
Daniyalk0 <getdaniyalkhan@gmail.com>
2025-07-12 14:39:45 +0530

    fix

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,8 +1,14 @@
-// components/theme-provider.tsx
-'use client'
+"use client";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
+import { ReactNode } from "react";
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+type Props = ThemeProviderProps & {
+  children: ReactNode;
+};
 
-export function ThemeProvider({ children, ...props }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: Props) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
